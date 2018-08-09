6d14eafe54b52869b59d77f546865749e1690e15
liuzw <670115782@qq.com>
2018-08-09 14:02:57 +0800

    修改代码

--- src/main/java/com/liuzw/generate/GenerateApplication.java
+++ src/main/java/com/liuzw/generate/GenerateApplication.java
@@ -2,6 +2,8 @@ package com.liuzw.generate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -9,7 +11,12 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @MapperScan("com.liuzw.generate.mapper")
 @SpringBootApplication
-public class GenerateApplication {
+public class GenerateApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GenerateApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GenerateApplication.class, args);
